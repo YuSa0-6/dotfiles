09f89dc1aa257ab7fc15d4b71609bdb988920e9f
YuSa0-6 <fuku.sugar@icloud.com>
2026-09-20 20:54:06 +0900

    chore(nvim): add editor configuration

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- Neovim 0.12 ネイティブ・ミニマル構成
+-- vim.pack + vim.lsp.config + blink.cmp + mini.nvim
+require("options")
+require("ui")
+require("pack")
+require("autocmds")
+require("diagnostic")
+require("keymaps")
+require("lsp")
